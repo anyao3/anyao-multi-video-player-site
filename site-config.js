2aa4ce3b0da96fc84d4e7d6a38c044694216a634
anyao3 <anyao3@users.noreply.github.com>
2026-08-16 21:10:30 +0900

    Remove footer GitHub link

--- site-config.js
+++ site-config.js
@@ -1,10 +1,8 @@
-const REPOSITORY_URL = "https://github.com/anyao3/anyao-multi-video-player-site";
 const MAC_ARM64_DOWNLOAD_URL = "https://github.com/anyao3/anyao-multi-video-player-site/releases/download/v0.3.3/Anyao.Multi.Video.Player-0.3.3-arm64.dmg";
 const MAC_X64_DOWNLOAD_URL = "https://github.com/anyao3/anyao-multi-video-player-site/releases/download/v0.3.3/Anyao.Multi.Video.Player-0.3.3-x64.dmg";
 const WINDOWS_DOWNLOAD_URL = "https://github.com/anyao3/anyao-multi-video-player-site/releases/download/v0.3.3/Anyao.Multi.Video.Player.Setup.0.3.3.exe";
 const CHECKOUT_URL = "https://anyao.lemonsqueezy.com/checkout/buy/0c60c071-38d3-4b6e-9963-ab38040d32fe";
 
-document.querySelectorAll(".repo-link").forEach((link) => { link.href = REPOSITORY_URL; });
 document.querySelectorAll(".download-mac-arm64").forEach((link) => { link.href = MAC_ARM64_DOWNLOAD_URL; });
 document.querySelectorAll(".download-mac-x64").forEach((link) => { link.href = MAC_X64_DOWNLOAD_URL; });
 document.querySelectorAll(".download-windows").forEach((link) => { link.href = WINDOWS_DOWNLOAD_URL; });
